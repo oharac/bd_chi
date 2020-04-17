a91cf9d53bb2a3bf0574b814a3b821235d13be22
Casey O'Hara <ohara@nceas.ucsb.edu>
2020-04-16 19:38:13 -0700

    reran maps of impact on taxa, filtered to just seabirds, working on trends

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 To do:
 
-* [ ] Update species list and maps for IUCN API version 2020-1
-* [ ] Check stressor maps for proper calculation of 95% contour volume
+* [X] Update species list and maps for IUCN API version 2020-1
+    * [ ] Add a marine birds filter into the habitats script early on.
+* [X] Check stressor maps for proper calculation of 95% contour volume
     * [X] Clip all stressor year ranges to 2002-2013
     * [X] Ocean acidification in particular!
     * [X] While you're at it, crop the SLR raster to just coastal areas!
@@ -17,8 +18,9 @@ To do:
     * [ ] Refugia range as (whole range - impacted range)
     * [ ] Account for ocean cell area in calculations!
 * From intersections, map cumulative impacts on threatened and near-threatened spp
-    * [ ] per cell: number of species impacted; proportion of species impacted
-    * [ ] per cell: number of species in refugia; proportion of species in refugia
+    * [X] per cell: number of species impacted; proportion of species impacted
+    * [X] per cell: number of species in refugia; proportion of species in refugia
+* [ ] Check (in `plot_nspp_str_intsx.Rmd`) certain birds have greater marine ranges than total ranges!
 * Plot ideas:
     * [ ] Allan et al 2019, Fig. 3 (# of impacted spp)
     * [ ] Allan et al 2019, Fig. 4 (# of unimpacted spp) 
@@ -26,7 +28,18 @@ To do:
     * [ ] compare range size vs. % area impacted (compare to protection targets differentiated by range size)
     * [ ] compare threat status vs. % area impacted a la Allan et al. Fig. 2
     * [ ] compare important threats by taxonomic group?
-    
+* [ ] Look into Jenkins methods and refs:
+    * [ ] Woods et al 2008
+    * [ ] Jetz e al 2008, Loiselle et al 2003 (and others from IUCN/AM)
+    * [ ] Turtles - Halpin 2009, Kot 2015
+    * [ ] See how they assessed "marine impact per species" - "we calculated from Halpern et al (2015a) the mean value that occurred within the species range" - the mean value of total impact? any reference to sensitivity/spp-specific impacts?
+    * [ ] they include threatened, DD (can I do this? not to find specific impacts), and small ranges - how is small range defined?
+    * [ ] Priority score: proportion of spp range unprotected divided by ln(range area):
+        * increases as range size decreases "in accordance with the well-established relationship between range area and extinction risk" (Manne and Pimm 2001; Manne et al 1999; Purvis et al 2000)
+        * for each cell, id which species occur, and sum their priority scores
+* [ ] Some sort of visualization/chart of how many species in each taxa and/or range size class are impacted by various stressors 
+    * [ ] e.g. small scale spp threatened by cc vs large scale threatened by fishing
+
 Scripts for CHI 2019: https://github.com/OHI-Science/impact_acceleration
 
 Stressor files are on Mazu: M:/git-annex/impact_acceleration/stressors
--- common_fxns.R
+++ common_fxns.R
@@ -31,8 +31,7 @@ message <- function(x, ...) {
 }
 
 ### Get API version
-
-### api_key stored on git-annex so outside users can use their own key
+### * api_key stored on git-annex so outside users can use their own key
 api_file <- file.path(dir_M, 'git-annex/globalprep/spp_ico', 
                       'api_key.csv')
 api_key <- scan(api_file, what = 'character')
@@ -46,3 +45,64 @@ if(api_version_current != api_version) {
   message('IUCN API at ', api_version, '; most current version.')
 }
 
+### Create table of species included in this assessment
+### * Threatened or near threatened
+### * Mapped
+### * Comprehensively assessed taxa
+### * threatened by one or more stressors (flag)
+
+get_incl_spp <- function(api_V = api_version, include_all_risk = FALSE) {
+  ### Load spp info for sensitivity, risk, comprehensive status, and maps.
+  ### Also filters to seabirds more specifically.
+  
+  sens_file <- here('_output', sprintf('spp_sensitivity_%s.csv', api_V))
+  risk_file <- here('_data', sprintf('iucn_risk_current_%s.csv', api_V))
+  maps_file <- here('_data', sprintf('spp_marine_maps_%s.csv', api_V))
+  comp_file <- here('_data', sprintf('iucn_comp_assessed_%s.csv', api_V))
+  bird_file <- here('_raw', 'seabird_species_2019.csv')
+  
+  spp_maps <- read_csv(maps_file, col_types = cols('iucn_sid' = 'i',
+                                                   'subpop'   = 'c')) %>%
+    filter(presence != 5) %>%
+    select(iucn_sid) %>% ### just need ID numbers here
+    distinct()
+  
+  spp_risk <- read_csv(risk_file, col_types = cols('iucn_sid' = 'i')) %>%
+    select(iucn_sid, sciname, comname, cat_score)
+  
+  if(!include_all_risk) {
+    spp_risk <- spp_risk %>%
+      filter(!is.na(cat_score) & !cat_score %in% c(0, 1))
+  }
+  
+  spp_sens <- read_csv(sens_file, col_types = cols('iucn_sid' = 'i')) %>%
+    filter(sens == TRUE) %>%
+    select(-sens)
+  
+  spp_comp <- read_csv(comp_file, col_types = cols('iucn_sid' = 'i')) %>%
+    select(-sciname)
+  
+  seabirds <- read_csv(bird_file) %>%
+    select(sciname = scientific, comname = common, seabird = seabird_all)
+  
+  spp_incl <- spp_maps %>%
+    inner_join(spp_risk, by = 'iucn_sid') %>%
+      ### if include_all_risk == FALSE, drop LC, EX, DD
+    left_join(spp_comp, by = c('iucn_sid')) %>%
+    filter(!is.na(taxon)) %>%
+      ### drop non-comp-assessed
+    left_join(seabirds, by = c('sciname', 'comname')) %>%
+    filter(!(taxon == 'birds' & is.na(seabird))) %>%
+      ### drop birds that aren't seabirds
+    select(-seabird) %>%
+    left_join(spp_sens, by = 'iucn_sid') %>%
+      ### add stressor sensitivities; NA means not sensitive
+    distinct()
+  
+  # spp_incl$iucn_sid %>% unique() %>% length()
+  # spp_incl %>% filter(!is.na(stressor)) %>% .$iucn_sid %>% unique() %>% length()
+  ### 1273 species included here: mapped, threatened/NT, comp assessed.
+  ### * 1006 of which are sensitive to one or more stressors
+  return(spp_incl)
+  
+}
